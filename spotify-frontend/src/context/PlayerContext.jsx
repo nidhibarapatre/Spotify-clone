65c7e91fdc6b231d4442a3583705f1d05710fcca
nidhibarapatre <116302859+nidhibarapatre@users.noreply.github.com>
2025-03-02 01:18:27 +0530

    Update PlayerContext.jsx

--- spotify-frontend/src/context/PlayerContext.jsx
+++ spotify-frontend/src/context/PlayerContext.jsx
@@ -5,7 +5,7 @@ export const PlayerContext = createContext();
 
 const PlayerContextProvider = (props) => {
 
-    const url = 'http://localhost:3000';
+    const url = 'https://spotify-clone-backend-nxf2.onrender.com';
 
     const audioRef = useRef();
     const seekBar = useRef();
